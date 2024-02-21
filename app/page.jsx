3f62104a88d842961cfd077e4d708b81f0d33abc
harshahossain <harshahossain@gmail.com>
2024-02-21 21:55:37 +0600

    updated homescreen

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,15 @@
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <section className="w-full flex-col flex-center">
+      <h1 className="head_text text-center">
+        Discover & Share <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Prompt Palace is an open-source AI prompting tool for modern world to
+        discover, create and share creative prompts
+      </p>
+      {/* feed */}
+    </section>
+  );
 }
