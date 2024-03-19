a43aa56fe7c1d0fae11fbc8cf019d3203e61a106
harshahossain <harshahossain@gmail.com>
2024-03-19 21:29:26 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -21,3 +21,23 @@ const nextConfig = {
 //     domains: ["lh3.googleusercontent.com"],
 //   },
 export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   experimental: {
+//     appDir: true,
+//     serverComponentsExternalPackages: ["mongoose"],
+//   },
+//   images: {
+//     domains: ["lh3.googleusercontent.com"],
+//   },
+//   webpack(config) {
+//     config.experiments = {
+//       ...config.experiments,
+//       topLevelAwait: true,
+//     };
+//     return config;
+//   },
+// };
+
+// export default nextConfig;
